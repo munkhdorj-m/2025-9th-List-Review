7dd96ad103fd127bfa91b4b81483e3465af7aa76
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-04-22 10:49:22 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,16 +1,49 @@
 # You can remove 'pass' if you written code in the function 
 
 # Exercise 1
-def find_average(numbers):
-    # Write your code here
-    pass
+def sum_array(lst):
+    total = 0
+    for num in lst:
+        total += num
+    return total
+
 
 # Exercise 2
-def multiply_elements(numbers):
-    # Write your code here
-    pass
+def sum_positive(lst):
+    total = 0
+    for num in lst:
+        if num > 0:
+            total += num
+    return total
+
 
 # Exercise 3
-def find_smallest(numbers):
-    # Write your code here
-    pass
+def more_odds(lst):
+    odd = 0
+    even = 0
+    for num in lst:
+        if num % 2 == 0:
+            even += 1
+        else:
+            odd += 1
+    if odd > even:
+        return "YES"
+    return "NO"
+
+
+# Exercise 4
+def find_max(lst):
+    maximum = lst[0]
+    for num in lst:
+        if num > maximum:
+            maximum = num
+    return maximum
+
+
+# Exercise 5
+def count_target(lst, target):
+    count = 0
+    for num in lst:
+        if num == target:
+            count += 1
+    return count
