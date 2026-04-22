94371c4869e830bbcf4d6e8f34c1701dc70c08a9
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2026-04-22 10:51:21 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -2,47 +2,22 @@
 
 # Exercise 1
 def sum_array(lst):
-    total = 0
-    for num in lst:
-        total += num
-    return total
-
+    pass 
+    
 # Exercise 2
 def sum_positive(lst):
-    total = 0
-    for num in lst:
-        if num > 0:
-            total += num
-    return total
+    pass
 
 
 # Exercise 3
 def more_odds(lst):
-    odd = 0
-    even = 0
-    for num in lst:
-        if num % 2 == 0:
-            even += 1
-        else:
-            odd += 1
-    if odd > even:
-        return "YES"
-    return "NO"
+    pass
 
 
 # Exercise 4
 def find_max(lst):
-    maximum = lst[0]
-    for num in lst:
-        if num > maximum:
-            maximum = num
-    return maximum
-
+    pass
 
 # Exercise 5
 def count_target(lst, target):
-    count = 0
-    for num in lst:
-        if num == target:
-            count += 1
-    return count
+    pass
